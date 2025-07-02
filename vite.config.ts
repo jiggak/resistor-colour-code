08a28a3146cc1346c4b1eda0c6f84964309041c8
Josh Kropf <josh@slashdev.ca>
2025-07-02 16:13:06 -0400

    Add favicon

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-   plugins: [svelte(), tailwindcss()]
+   plugins: [svelte(), tailwindcss()],
+   build: {
+      sourcemap: true
+   }
 });
